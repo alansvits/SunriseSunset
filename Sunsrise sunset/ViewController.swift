c4002d24e552c24e3bc9572fbc08d79f8449a2d8
Stas Shetko <alansvits@macbook.local>
2018-11-19 18:35:14 +0200

    Initial Commit

---
+++ Sunsrise sunset/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sunsrise sunset
+//
+//  Created by Stas Shetko on 4/11/18.
+//  Copyright © 2018 Stas Shetko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
